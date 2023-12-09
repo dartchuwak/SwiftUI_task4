12d3768084528df236d1c65b160d41896991b6e4
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-09 22:14:04 +0300

    Initial Commit

---
+++ SwiftUI_task4/SwiftUI_task4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_task4App.swift
+//  SwiftUI_task4
+//
+//  Created by Evgenii Mikhailov on 09.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_task4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
